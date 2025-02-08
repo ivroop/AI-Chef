ec91876f4a0f54c2a29a52e21355521c46389586
ivroop <poorvitandel01@gmail.com>
2025-02-08 17:25:01 +0530

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import chefClaudeLogo from "../assets/chef-claude-icon.png"
+const Header = () => {
+    return(
+        <header>
+            <img src={chefClaudeLogo} alt="logo" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
+
+export default Header;
