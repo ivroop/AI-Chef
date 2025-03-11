6e92be8d04bad18cace7f8731dfa4a8cb8aa43cf
ivroop <poorvitandel01@gmail.com>
2025-03-11 22:30:24 +0530

    modify code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,29 @@
 const Main = () => {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+    function handleSubmit(){
+        console.log("Form Submitted!")
+    }
+
     return(
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" placeholder="e.g. oregano"/>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input 
+                    type="text" 
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
                 <button>
                     Add ingredient
                 </button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
